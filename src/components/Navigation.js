4932337f7ae3d569c38a492b606665f16ea7ba0f
NilangaJayawickrama <nilangajayawickrama1118@gmail.com>
2024-05-29 09:58:59 +0530

    Added responsiveness

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Navigation() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const scrollToSection = (sectionId) => {
         const section = document.getElementById(sectionId);
         if (section) {
+            setIsMenuOpen(false);
             section.scrollIntoView({ behavior: 'smooth' });
         }
+    };
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
     };
 
     return (
@@ -15,75 +21,78 @@ function Navigation() {
                 <div className='flex items-center'>
                     <h1 className='mx-4 text-3xl font-kaushan text-customTeal'>Nilanga</h1>
                 </div>
-                <ul className='flex text-white'>
-                    <li onClick={() => scrollToSection('introduction')} className='mx-4 cursor-pointer hover:text-customTeal'>
-                        Home
-                    </li>
-                    <li onClick={() => scrollToSection('aboutMe')} className='mx-4 cursor-pointer relative group hover:text-customTeal'>
-                        About Me
-                    </li>
-                    <li onClick={() => scrollToSection('skills')} className='mx-4 cursor-pointer relative group hover:text-customTeal'>
-                        Skills
-                    </li>
-                    <li onClick={() => scrollToSection('projects')} className='mx-4 cursor-pointer relative group hover:text-customTeal'>
-                        Projects
-                    </li>
-                    <li onClick={() => scrollToSection('experience')} className='mx-4 cursor-pointer relative group hover:text-customTeal'>
-                        Experience
-                    </li>
-                    <li onClick={() => scrollToSection('testimonials')} className='mx-4 cursor-pointer relative group hover:text-customTeal'>
-                        Testimonials
-                    </li>
-                    <li onClick={() => scrollToSection('blogs')} className='mx-4 cursor-pointer relative group hover:text-customTeal'>
-                        Blogs
-                    </li>
-                    <li onClick={() => scrollToSection('contactMe')} className='mx-4 cursor-pointer relative group hover:text-customTeal'>
-                        Contact Me
-                    </li>
-                </ul>
+                <div className="lg:hidden">
+                    <button onClick={toggleMenu} className="text-white focus:outline-none">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
+                        </svg>
+                    </button>
+                </div>
+                {/* Header elements for large screens */}
+                {!isMenuOpen && (
+                    <ul className='lg:flex hidden text-white'>
+                        <li onClick={() => scrollToSection('introduction')} className='mx-4 cursor-pointer hover:text-customTeal'>
+                            Home
+                        </li>
+                        <li onClick={() => scrollToSection('aboutMe')} className='mx-4 cursor-pointer relative group hover:text-customTeal'>
+                            About Me
+                        </li>
+                        <li onClick={() => scrollToSection('skills')} className='mx-4 cursor-pointer relative group hover:text-customTeal'>
+                            Skills
+                        </li>
+                        <li onClick={() => scrollToSection('projects')} className='mx-4 cursor-pointer relative group hover:text-customTeal'>
+                            Projects
+                        </li>
+                        <li onClick={() => scrollToSection('experience')} className='mx-4 cursor-pointer relative group hover:text-customTeal'>
+                            Experience
+                        </li>
+                        <li onClick={() => scrollToSection('testimonials')} className='mx-4 cursor-pointer relative group hover:text-customTeal'>
+                            Testimonials
+                        </li>
+                        <li onClick={() => scrollToSection('blogs')} className='mx-4 cursor-pointer relative group hover:text-customTeal'>
+                            Blogs
+                        </li>
+                        <li onClick={() => scrollToSection('contactMe')} className='mx-4 cursor-pointer relative group hover:text-customTeal'>
+                            Contact Me
+                        </li>
+                    </ul>
+                )}
+                {/* End of the header elements for large screens */}
+
+                {/* Dropdown menu for small screens */}
+                {isMenuOpen && (
+                    <div className="lg:hidden absolute top-[60px] right-0 left-0 bg-gray-800">
+                        <ul className="flex flex-col text-white">
+                            <li onClick={() => scrollToSection('introduction')} className='mx-4 my-2 cursor-pointer hover:text-customTeal'>
+                                Home
+                            </li>
+                            <li onClick={() => scrollToSection('aboutMe')} className='mx-4 my-2 cursor-pointer hover:text-customTeal'>
+                                About Me
+                            </li>
+                            <li onClick={() => scrollToSection('skills')} className='mx-4 my-2 cursor-pointer hover:text-customTeal'>
+                                Skills
+                            </li>
+                            <li onClick={() => scrollToSection('projects')} className='mx-4 my-2 cursor-pointer hover:text-customTeal'>
+                                Projects
+                            </li>
+                            <li onClick={() => scrollToSection('experience')} className='mx-4 my-2 cursor-pointer hover:text-customTeal'>
+                                Experience
+                            </li>
+                            <li onClick={() => scrollToSection('testimonials')} className='mx-4 my-2 cursor-pointer hover:text-customTeal'>
+                                Testimonials
+                            </li>
+                            <li onClick={() => scrollToSection('blogs')} className='mx-4 my-2 cursor-pointer hover:text-customTeal'>
+                                Blogs
+                            </li>
+                            <li onClick={() => scrollToSection('contactMe')} className='mx-4 my-2 cursor-pointer hover:text-customTeal'>
+                                Contact Me
+                            </li>
+                        </ul>
+                    </div>
+                )}
+                {/* End of dropdown menu for small screens */}
+
             </div>
-
-            {/* <ul className={!nav ? 'hidden' : 'absolute bg-zinc-200 w-full px-8'}>
-                <li onClick={handleClose} className='border-b-2 border-zinc-300 w-full'>
-                    Home
-                </li>
-
-                <li onClick={handleClose} className='border-b-2 border-zinc-300 w-full'>
-                    About Us
-                </li>
-
-                <li onClick={handleClose} className='border-b-2 border-zinc-300 w-full'>
-                    Contact Us
-                </li>
-
-                <li onClick={handleClose} className='border-b-2 border-zinc-300 w-full'>
-                    Services
-                </li>
-            </ul> */}
-
-            {/* <div className='md:hidden mr-4' onClick={handleClick}>
-                {!nav ? <MenuIcon className='w-5 text-black' /> : <div className='flex'>
-
-                    <a
-                        className="flex text-center cursor-pointer items-center mx-4 text-black hover:text-green-600"
-                        type="submit"
-                    >
-                        <FaSignInAlt className='lg:w-5 lg:h-5 mx-2' />
-                        <span className="text-sm font-medium">
-                            Login
-                        </span>
-                    </a>
-
-                    <a
-                        className="block cursor-pointer shrink-0 rounded-lg bg-white mr-4 p-2.5 border border-gray-100 shadow-sm hover:bg-transparent hover:text-green-600 hover:border hover:border-green-600"
-                    >
-                        <span className="sr-only">Account</span>
-                        <FaUserAlt className='lg:w-5 lg:h-5' />
-                    </a>
-                    <XIcon className='w-5 text-black' />
-                </div>}
-
-            </div> */}
         </div>
     )
 }
