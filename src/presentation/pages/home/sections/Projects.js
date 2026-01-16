fcab2f2cc4dfa495251d6763ceee4ea4907e0547
NilangaJayawickrama <jayawickrama.np@gmail.com>
2026-01-16 15:53:14 -0800

    Added project section

--- src/presentation/pages/home/sections/Projects.js
+++ src/presentation/pages/home/sections/Projects.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const Projects = () => {
-    return (
-        <div className="h-64 bg-gray-200">
-            <h1 className='text-black'>Projects</h1>
-        </div>
-    );
-}
+  return (
+    <section className="pt-[70px] flex justify-center items-center py-10 sm:py-12 md:py-16 bg-[#111827]">
+      <div className="text-white max-w-2xl lg:max-w-6xl text-center lg:text-left flex-1 px-2 lg:px-0">
+
+        {/* Title */}
+        <p className="text-xs sm:text-sm lg:text-base font-semibold tracking-[0.2em] uppercase text-gray-300 mb-2">
+          Prjects
+        </p>
+
+      </div>
+    </section>
+  );
+};
 
 export default Projects;
